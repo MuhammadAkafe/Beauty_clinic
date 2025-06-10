6ba9dd8115c5df8edbeb9b43a6a19fb94c97cd4b
muhammad aqfa <allappsdata97@gmail.com>
2025-06-11 02:31:09 +0300

    fc

--- src/axios_instance.ts
+++ src/axios_instance.ts
@@ -12,5 +12,7 @@ const axiosInstance = axios.create({
     },
 });
 
+
+
 export default axiosInstance;
 
---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+    // Load env file based on `mode` in the current directory.
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+        plugins: [react()],
+        base: '/',
+        // Expose env variables to your client-side code
+        envPrefix: 'VITE_',
+        // Define global constants
+        define: {
+            'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
+        }
+    };
+});
