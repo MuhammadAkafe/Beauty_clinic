df4b708b3916c29ae00ef1bbacfb1d6564e52bfa
muhammad aqfa <allappsdata97@gmail.com>
2025-06-13 04:31:56 +0300

    Update Vite configuration to use babel-plugin-react-compiler for enhanced React support, add babel-plugin-react-compiler dependency, and remove legacy vite.config.js file.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
+
+const ReactCompilerConfig = { 
+  development: true,
+  production: false
+}
+
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current directory.
   const env = loadEnv(mode, process.cwd(), '')
   
   return {
-    plugins: [react()],
+    plugins: [react({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler', ReactCompilerConfig]
+        ],
+      },
+    })
+
+    ],
     base: '/',
     // Expose env variables to your client-side code
     envPrefix: 'VITE_',
